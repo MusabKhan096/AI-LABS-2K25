4b35f001c043748cdbee73bccd174c098221d046
Musa'b Khan <m.ksoomrah@gmail.com>
2025-12-14 19:33:45 +0500

    Delete Lab-05 directory

--- Lab-05/Lab05T1.py
+++
@@ -1,5 +0,0 @@
-arr = [[0, 1], [2, 3]]
-flat = [0, 1, 2, 3]
-print("Original flattened array:", arr)
-print("Maximum value of the above flattened array:", max(flat))
-print("Minimum value of the above flattened array:", min(flat))
