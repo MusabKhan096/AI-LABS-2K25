f40157e2cf5309c38697da8fac4760020b992937
Musa'b Khan <m.ksoomrah@gmail.com>
2025-12-14 19:35:19 +0500

    Add files via upload

---
+++ Lab - 05/Lab05T3.py
@@ -0,0 +1,5 @@
+import numpy as np
+print("=============")
+arr = np.array([1, 2, 3, 4])
+print(arr.astype(float))
+print("=============")
---
+++ Lab - 05/Lab05T5.py
@@ -0,0 +1,3 @@
+import numpy as np
+A = np.array([[6, 1, 1, 3], [4, -2, 5, 1],[2, 8, 7, 6],[3, 1, 9, 7]])
+print(np.linalg.inv(A))
