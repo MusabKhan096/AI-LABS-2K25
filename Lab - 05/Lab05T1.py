4a0f8a2ae014920d9268d523a2100879a20219be
Musa'b Khan <m.ksoomrah@gmail.com>
2025-12-14 19:34:38 +0500

    Add Lab05T1.py to print array details

---
+++ Lab - 05/Lab05T1.py
@@ -0,0 +1,5 @@
+arr = [[0, 1], [2, 3]]
+flat = [0, 1, 2, 3]
+print("Original flattened array:", arr)
+print("Maximum value of the above flattened array:", max(flat))
+print("Minimum value of the above flattened array:", min(flat))
